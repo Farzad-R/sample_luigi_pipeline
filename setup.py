c8f3791d4aa9bc63092d12af76d2cf039bb214ef
Farzad-R <farzad.roozitalab@gmail.com>
2022-04-02 22:39:03 -0400

    modified setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "wind_power_prediction",
+    name = "sample_luigi_pipeline",
     version = "0.1.0",
-    description = "prediction of output power of Ontario's wind farms",
+    description = "A sample pipeline using luigi, pip, and os",
     authors = ["Farzad Roozitalab <farzadroozitalab@cmail.carleton.ca>"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Farzad-R/wind_power_prediction",
+    url="https://github.com/Farzad-R/sample_luigi_pipeline",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
